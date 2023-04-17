f7116080c9466d1b0bd31052bf479310ee5d14a9
Usama Zahid <usamazahid100@gmail.com>
2023-04-17 06:00:57 +0500

    changed limit

--- src/app/api/helpers/limiter.tsx
+++ src/app/api/helpers/limiter.tsx
@@ -1,7 +1,7 @@
 import { RateLimiter } from "limiter";
 
 export const limiter = new RateLimiter({
-    tokensPerInterval:3,
+    tokensPerInterval:100,
     interval:"min",
     fireImmediately:true,
 })
